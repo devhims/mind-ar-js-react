eaf027fbd97f09071aa5d86df533f1006abdcf1b
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-03-16 12:33:15 +0530

    project cleanup and force https in start script

--- src/App.js
+++ src/App.js
@@ -7,18 +7,39 @@ function App() {
   const [started, setStarted] = useState(false);
   return (
     <div className="App">
-      <h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a></h1>
+      <h1>
+        Example React component with{' '}
+        <a href="https://github.com/hiukim/mind-ar-js" target="_blank">
+          MindAR
+        </a>
+      </h1>
 
       <div>
-	{!started && <button onClick={() => {setStarted(true)}}>Start</button>}
-	{started && <button onClick={() => {setStarted(false)}}>Stop</button>}
+        {!started && (
+          <button
+            onClick={() => {
+              setStarted(true);
+            }}
+          >
+            Start
+          </button>
+        )}
+        {started && (
+          <button
+            onClick={() => {
+              setStarted(false);
+            }}
+          >
+            Stop
+          </button>
+        )}
       </div>
 
       {started && (
-	<div className="container">
-	  <MindARViewer/>
-	  <video></video>
-	</div>
+        <div className="container">
+          <MindARViewer />
+          <video></video>
+        </div>
       )}
     </div>
   );
